6b9ba6927cedb65143f3d02b62449bc0100de07e
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-06-29 21:52:52 -0500

    dun

--- src/index.js
+++ src/index.js
@@ -11,7 +11,31 @@ const transform = pipe(trim, toLowerCase, wrap("div"))
 
 console.log(transform(input))
 
+const person = { name: "john"}
 
+const updated = Object.assign({}, person, {name: "Bob", age: 39})
 
+const spreadPerson = {
+    ...person,
+    address: {
+        ...person.address,
+        city: "New York"
+    },
+    name: "Bobby"
+}
+
+console.log(person, spreadPerson)
+
+const nums = [1,2,3]
+const index = nums.indexOf(2)
+const spliceAdded = [
+    ...nums.slice(0, index),
+4, ...nums.slice(index)
+]
+const added = [...nums]
+
+console.log(spliceAdded)
+
+console.log(spliceAdded.filter(n => n != 2))
 
 
