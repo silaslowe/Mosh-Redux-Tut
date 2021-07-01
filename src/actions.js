cc563f818b77124b3f27e9e980d6d5dbe86cee10
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-01 10:49:46 -0500

    added resolve bug to actions  and store

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,17 @@
-function bugAdded(desc) {
+import * as actions from './actionTypes'
+
+export function bugAdded(desc) {
     return {
         type: actions.BUG_ADDED,
         payload: {
-            desc: "Bug1"
+            desc: desc
         }
     }
 }
+
+export const bugResolved = (id) => ({
+    type: actions.BUG_RESOLVED,
+    payload: {
+        id
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import store from "./store";
-import * as actions from './actionTypes'
-import {bugAdded} from './actions'
+import {bugAdded, bugResolved} from './actions'
 
 const unsubscribe = store.subscribe(() =>
 console.log("Change", store.getState()))
 
-store.dispatch({
-    type: actions.BUG_ADDED,
-    payload: {
-        description: 'Bug1'
-    }
-})
+store.dispatch(bugAdded("Bug1"))
+store.dispatch(bugResolved(1))
 
 unsubscribe()
 
-store.dispatch({
-    type: actions.BUG_REMOVED,
-    payload: {
-        id: 1
-    }
-})
+// store.dispatch({
+//     type: actions.BUG_REMOVED,
+//     payload: {
+//         id: 1
+//     }
+// })
 
 console.log(store.getState())
 
