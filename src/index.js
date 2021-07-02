2ba745d068caffd2c95a5b2343276ce5c48f2704
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-02 09:04:21 -0500

    added project slice and changed index to add project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from "./store/configureStore.js";
-import * as actions from "./store/bugs"
+import * as actions from "./store/projects"
 
 const store = configureStore()
 
@@ -7,10 +7,13 @@ store.subscribe(() => {
     console.log("Store changed")
 })
 
-store.dispatch(actions.bugAdded({desc: "Bug1"}))
-store.dispatch(actions.bugAdded({desc: "Bug2"}))
-store.dispatch(actions.bugAdded({desc: "Bug3"}))
-store.dispatch(actions.bugResolved({id: 1}))
+// store.dispatch(actions.bugAdded({desc: "Bug1"}))
+// store.dispatch(actions.bugAdded({desc: "Bug2"}))
+// store.dispatch(actions.bugAdded({desc: "Bug3"}))
+// store.dispatch(actions.bugResolved({id: 1}))
+store.dispatch(actions.projectAdded({name: "Project1"}))
+
+
 
 console.log(store.getState())
 
