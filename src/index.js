aad408a2193d3d56b911838fd684836cfb4a7a4b
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-07 11:27:01 -0500

    added func middleware to call apis

--- src/index.js
+++ src/index.js
@@ -14,41 +14,9 @@ import {userAdded} from "./store/users";
 
 const store = configureStore()
 
-store.subscribe(() => {
-    console.log("Store changed")
+store.dispatch((dispatch, getState) => {
+    dispatch({type: 'bugsReceived', bugs: [1,2,3]})
+    console.log(store)
 })
-
-// store.dispatch(bugAdded({
-//     desc: "Bug1",
-//     team_member: 2
-// }))
-// store.dispatch(bugAdded({
-//     desc: "Bug2",
-//     team_member: 1
-// }))
-// store.dispatch(bugAdded({
-//     desc: "Bug3",
-//     team_member: 2
-// }))
-store.dispatch(userAdded({name: "User1"}))
-// store.dispatch(userAdded({name: "User2"}))
-// store.dispatch(bugResolved({id: 1}))
-// store.dispatch(bugAssignedToUser({
-//     bugId: 1,
-//     userId: 1
-// }))
-//
-// store.dispatch(bugAssignedToUser({
-//     bugId: 2,
-//     userId: 1
-// }))
-// store.dispatch(projectAdded({name: "Project1"}))
-
-const unresolvedBugs = getUnresolvedBugs(store.getState())
-const bugs = getBugsByUser(1)(store.getState())
-
-console.log("BUGS", bugs)
-console.log("UR", unresolvedBugs)
-console.log(store.getState())
 
 
